7e41d52846f121f054d0e86b2318ac14e6a0a22b
Eunbit Kim <eunbitkim90@gmail.com>
2019-01-09 12:02:13 +0000

    done

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,18 +1,34 @@
 class GildedRose
+class Item
+    attr_accessor :name, :sell_in, :quality
 
-  def initialize(items)
-    @items = items
-  end
+    # def initialize(name, sell_in, quality)
+    #   @name = name
+    #   @sell_in = sell_in
+    #   @quality = quality
+    # end
+
+    # def to_s()
+    #   "#{@name}, #{@sell_in}, #{@quality}"
+    # end
+  # end
+
+   attr_writer :items
+  # def initialize(items)
+  #   @items = items
+  # end
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if item.name != "Aged Brie", "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
             item.quality = item.quality - 1
           end
         end
-      else
+
+    else
+
         if item.quality < 50
           item.quality = item.quality + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
@@ -29,9 +45,11 @@ class GildedRose
           end
         end
       end
+
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
       end
+
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
@@ -40,6 +58,7 @@ class GildedRose
                 item.quality = item.quality - 1
               end
             end
+
           else
             item.quality = item.quality - item.quality
           end
@@ -52,18 +71,3 @@ class GildedRose
     end
   end
 end
-
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
-  end
-end
-
